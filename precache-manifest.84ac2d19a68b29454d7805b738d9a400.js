d8cdaabe8c52c50453a69f917b9d3942418054af
ad-ahanadutta <ahanadutta555@gmail.com>
2020-10-19 14:16:17 +0530

    Updates

---
+++ precache-manifest.84ac2d19a68b29454d7805b738d9a400.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad09cc554ae0a147ef5a57461d2b3e76",
+    "url": "/Get-Weather/index.html"
+  },
+  {
+    "revision": "854563b73fb2e3a187eb",
+    "url": "/Get-Weather/static/css/main.0af91ad5.chunk.css"
+  },
+  {
+    "revision": "ccc7afc3307c62f7dabf",
+    "url": "/Get-Weather/static/js/2.6475ed6c.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Get-Weather/static/js/2.6475ed6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "854563b73fb2e3a187eb",
+    "url": "/Get-Weather/static/js/main.a2cb57c1.chunk.js"
+  },
+  {
+    "revision": "698c8a4f365a11e9ead1",
+    "url": "/Get-Weather/static/js/runtime-main.7b90c049.js"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/Get-Weather/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/Get-Weather/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
